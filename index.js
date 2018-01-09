a23370a41ab43402b6f16c2c9819e17bc60e162b
Loree Nichols <loree.d.nichols@gmail.com>
2018-01-09 10:58:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
   return "I can't hear you!"
 }
-funtion sayHiToGrandma(string) {
+function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
   return "YES INDEED"
 }
