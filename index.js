b4ce4e82fe4e3fbe05f8641571860c0ed97a97b7
Loree Nichols <loree.d.nichols@gmail.com>
2018-01-09 10:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function shout(string)  {
-return string
+
+function shout(string) {
+return string.toUpperCase()
 }
-
-
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  return string.toUpperCase()
+}
